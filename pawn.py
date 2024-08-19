88ba8b92410ab2e404d18e969c7895cbae5721a6
lzapata7 <l.zapata@alumno.um.edu.ar>
2024-08-18 23:07:37 -0300

    Peon

--- pawn.py
+++ pawn.py
@@ -1,5 +1,9 @@
 from piece import Piece
 
-
 class Pawn(Piece):
-    ...
+    def get_possible_moves(self, board, row, col):
+        moves = []
+        direction = -1 if self.color == 'white' else 1
+        if board[row + direction][col] is None:
+            moves.append((row + direction, col))
+        return moves
